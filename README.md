ae320fbefaedd20d203a269b4e0b5aa364d64bfa
Jakker1 <nicolasfvz@gmail.com>
2021-04-11 13:43:40 -0300

    Add maintain default viewmodel option

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Set '0' to disable the mod
 * Fixed thirdperson in vehicles<br/>
 * Fixed zoom with the HEV suit and sniper scopes
 * Add command 'setfov' to the console
+* Add maintain default viewmodel option
--- lua/autorun/client/fov.lua
+++ lua/autorun/client/fov.lua
@@ -4,12 +4,14 @@
 -- Create a console var for the custom fov --
 if CLIENT then
 	CreateClientConVar("cfov", 0)
+	CreateClientConVar("viewm_check", 0)
 end
 
 -- Create the Set FOV tab in the menu options --
 hook.Add( "AddToolMenuCategories", "menu_SetFOV", function()
 	spawnmenu.AddToolMenuOption("Utilities", "Custom FOV", "Set FOV", "Set FOV", "", "", function(panel)
 		panel:NumSlider( "FOV", "cfov", 85, 175, 0)
+		panel:CheckBox( 'Default viewmodel', 'viewm_check' )
 		panel:Help( "Set 0 to disable" )
 	end)
 end)
@@ -26,22 +28,39 @@ local function CalcNewFOV(ply)
 	return cfov_view
 end
 
+local function calc_viewfov(fov)
+	-- f(x) = ax + b
+	-- f(fov) = -0.994*fov + viewmodel_fov(=54)
+	return (fov-100) * -0.994 + 54
+end
 
 -- == ConVar 'cfov' CallBack == --
 -- Everytime the player changes his fov this function will be called --
 cvars.AddChangeCallback("cfov", function()
 	if GetConVarNumber("cfov") ~= 0 then
+
 		hook.Add("CalcView", "SetNewFOV", function(ply)
+			
 			if ply:InVehicle() then
 				if ply:GetVehicle():GetThirdPersonMode() then
 					return end
 			end
-	    	return CalcNewFOV(ply)
+
+			if GetConVarNumber("viewm_check") == 1 then
+				RunConsoleCommand("viewmodel_fov", calc_viewfov(GetConVarNumber('cfov')))
+			else
+				RunConsoleCommand("viewmodel_fov", 54)
+			end
+
+			return CalcNewFOV(ply)
+			
 		end)
+	
 	else
 		hook.Remove("CalcView", "SetNewFOV")
 	end
 end)
+
 
 -- Add command 'setfov' to the console --
 concommand.Add( "setfov", function( ply, cmd, args, argStr )
